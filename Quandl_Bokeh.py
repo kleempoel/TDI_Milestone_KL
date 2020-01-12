97b583a43a56c319961751df74c8e2d8f50f3b19
kleempoel <44100017+kleempoel@users.noreply.github.com>
2020-01-11 19:34:04 -0800

    make it better

--- Quandl_Bokeh.py
+++ Quandl_Bokeh.py
@@ -6,10 +6,34 @@ Created on Wed Jan  8 12:08:46 2020
 """
 
 #getting data from quandl
+import os
+os.chdir("C:\Data\The Data Incubator\TDI_Milestone_KL")
 import quandl
 import pandas as pd
 quandl.ApiConfig.api_key = 'G8fYyzS1iQS44dkAFC2x'
 #data=quandl.get('GDT/BUT_PI', start_date='2011-01-08', end_date='2020-01-08')
+
+#make list of dates
+import datetime
+#base = datetime.date.today()-datetime.timedelta(days=375) #going back 2 months because the data is not available for this year
+#numdays = 30
+#date_list = [base - datetime.timedelta(days=x) for x in range(numdays)]
+##get data for that list of dates
+#for x in range(numdays):
+#    newdate = date_list[x].strftime("%Y-%m-%d")
+#    data=quandl.get_table('SHARADAR/SEP', date=newdate, ticker='TSLA')
+#    print(newdate)
+#    print(data['close'])
+#
+#    
+#datetime.isoformat(date_list)
+#datetime.strptime(date_list[2], "%d/%m/%y")
+#date_list.strftime("%d/%m/%y")
+#data=quandl.get_table('SHARADAR/SEP', date=newdate, ticker='TSLA')
+#data=quandl.get_table('SHARADAR/SEP', date='2018-11-04', ticker='TSLA')
+#
+#data=quandl.get_table('SHARADAR/SEP', date='2018-12-31,2018-11-30', ticker='TSLA')
+#data2=quandl.get_table('SHARADAR/SEP', date='2018-11-30', ticker='TSLA')
 data=quandl.get("BATS/BATS_TSLA", start_date='2019-12-08', end_date='2020-01-08')
 pdata=pd.DataFrame(data=data)
 #pdata2=pdata[['Close']] 
--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import pandas as pd
 
 from bokeh.embed import components
 from bokeh.io import show, output_file
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, NumeralTickFormatter
 from bokeh.palettes import Spectral6
 from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
+from bokeh.models import DatetimeTickFormatter
+import quandl
+quandl.ApiConfig.api_key = 'G8fYyzS1iQS44dkAFC2x'
 
 app = Flask(__name__)
 
@@ -15,8 +18,17 @@ iris_df = pd.read_csv("data/iris.data",
     names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
 feature_names = iris_df.columns[0:-1].values.tolist()
 
+#Load stock data
+stock_current_feature_names=list(['TSLA','GOOG','INT','AMD'])
+import datetime
+today= datetime.date.today().strftime("%Y-%m-%d")
+inidate=(datetime.date.today()-datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+TSLA=pd.DataFrame(data=quandl.get("BATS/BATS_TSLA", start_date=inidate, end_date=today))
+GOOG=pd.DataFrame(data=quandl.get("BATS/BATS_GOOG", start_date=inidate, end_date=today))
+INT=pd.DataFrame(data=quandl.get("BATS/BATS_INT", start_date=inidate, end_date=today))
+AMD=pd.DataFrame(data=quandl.get("BATS/BATS_AMD", start_date=inidate, end_date=today))
 
-# Create the main plot
+# Create the main plot for iris
 def create_figure(current_feature_name, bins):
     bins=bins
     bins_val=pd.qcut(iris_df[current_feature_name], q=bins,precision=1).value_counts()
@@ -32,28 +44,56 @@ def create_figure(current_feature_name, bins):
     p.y_range.end = round(max(bins_count)*1.1,0)
     return p
 
+# Create the main plot for stock
+def create_figure_stock(stock_current_feature_name):
+    if stock_current_feature_name=='TSLA':
+        data_stock=TSLA
+    elif stock_current_feature_name=='GOOG':
+        data_stock=GOOG
+    elif stock_current_feature_name=='INT':
+        data_stock=INT
+    elif stock_current_feature_name=='AMD':
+        data_stock=AMD
+    p = figure(plot_width=600, plot_height=400, toolbar_location=None, 
+               title=stock_current_feature_name + " stock volume from Quandl (" + inidate + " - " + today + ")")
+    p.line(data_stock.index, data_stock["Total Volume"], line_width=2)
+    p.xaxis.formatter=DatetimeTickFormatter(
+            hours=["%d %B %Y"],
+            days=["%d %B %Y"],
+            months=["%d %B %Y"],
+            years=["%d %B %Y"],
+        )
+    p.xaxis.major_label_orientation = 3.14/4
+    p.yaxis.axis_label = "Total Volume"
+    p.yaxis.formatter=NumeralTickFormatter(format="00")
+    return p
+
+
 
 #Index page
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#@app.route('/Quandl_Tesla')
+#def Quandl_TSLA():
+#  return render_template('Quandl_TSLA.html')
 
-@app.route('/Quandl_Tesla')
+@app.route('/Quandl_Tesla_dyn')
 def Quandl_TSLA():
-  return render_template('Quandl_TSLA.html')
+    # Determine the selected feature
+	stock_current_feature_name = request.args.get("feature_name")
+	if stock_current_feature_name == None:
+		stock_current_feature_name = "TSLA"
+
+	# Create the plot
+	plot = create_figure_stock(stock_current_feature_name)
+		
+	# Embed plot into HTML via Flask Render
+	script, div = components(plot)
+	return render_template("Quandl_TSLA_dyn.html", script=script, div=div,
+		feature_names=stock_current_feature_names,  current_feature_name=stock_current_feature_name)
   
-@app.route('/notebook_embed')
-def notebook_embed():
-  return render_template('notebook_embed.html')
-
-@app.route('/bokeh_add_plot_no_server')
-def bokeh_add_plot_no_server():
-  return render_template('bokeh_add_plot_no_server.html')
-
 @app.route('/iris_index1')
 def iris_interactive_bokeh():
     # Determine the selected feature
@@ -68,6 +108,14 @@ def iris_interactive_bokeh():
 	script, div = components(plot)
 	return render_template("iris_index1.html", script=script, div=div,
 		feature_names=feature_names,  current_feature_name=current_feature_name)
+    
+@app.route('/notebook_embed')
+def notebook_embed():
+  return render_template('notebook_embed.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(port=5000)
